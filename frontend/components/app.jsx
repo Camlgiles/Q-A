cf37fcaf4db74009409ebfa89f1bc5eb2e9e9f77
Camlgiles <cgiles@gilescpa.com>
2019-07-30 16:31:50 -0700

    add authRoute and protectedRoute

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import {Route} from 'react-router-dom';
 import SignupContainer from './session/signup_container';
+import {AuthRoute, ProtectedRoute} from '../util/route_utils';
 
 export default () => (
    <div>
-      <Route path='/signup' component={SignupContainer} />
+      <AuthRoute path='/signup' component={SignupContainer} />
+      {/* <ProtectedRoute path='/questions' component={QuestionIndexContainer} />  */}
    </div>
 )
+
