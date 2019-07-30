29428706e8022de971284a74f2c81bcb6219775b
Camlgiles <cgiles@gilescpa.com>
2019-07-30 10:22:20 -0700

    add User model

--- frontend/entry.jsx
+++ frontend/entry.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 document.addEventListener('DOMContentLoaded', () => {
+   console.log('Hello');
    const root = document.getElementById('root');
    ReactDOM.render(<h1>Welcome to Q&amp;A</h1>, root);
 });
